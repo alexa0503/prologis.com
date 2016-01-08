c98784262d99f43660c45ae09e1ce80370f8fede
alexa0503 <lori.w@live.cn>
2016-01-08 17:38:45 +0800

    缩略图

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new BushidoIO\QRCodeBundle\BushidoIOQRCodeBundle(),
             new Stfalcon\Bundle\TinymceBundle\StfalconTinymceBundle(),
+            new Liip\ImagineBundle\LiipImagineBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
