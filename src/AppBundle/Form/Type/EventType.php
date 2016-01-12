4c4bc24102ba1b080ca1bd2ab28b763520275491
alexa0503 <lori.w@live.cn>
2016-01-12 18:25:07 +0800

    bug

--- src/AppBundle/Form/Type/EventType.php
+++ src/AppBundle/Form/Type/EventType.php
@@ -14,6 +14,7 @@ class EventType extends AbstractType
 			))
 			->add('subTitle', 'text', array(
 				'label' => '副标题',
+				'required' => false,
 			))
 			->add('info', 'textarea', array(
 				'label' => '内容',
